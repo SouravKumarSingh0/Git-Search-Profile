13d861a5af1221834a106e626563c7690f026aa3
Sourav Kumar Singh <ssourav474474@gmail.com>
2023-02-14 22:07:12 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+const  client_id = "";
+const client_secret = "";
+
+
+const fectch = async (user) => {
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
+    const data = await api_call.json();
+    return { data } 
+
+};
+
+const showdata = () => {
+    fectch(inputValue.value).then((res) => {
+        console.log(res);
+
+        nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+        unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+        reposContainer.innerHTML = `Repository: <span class="main__profile-value">${res.data.public_repos}</span>`;
+        urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.html_url}</span>`;
+    })
+};
+
+searchButton.addEventListener("click", () => {
+    showdata();
+})
